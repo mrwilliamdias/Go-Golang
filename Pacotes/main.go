b74f9d611522dc01e7ce4df101804629a4cebeed
mrwilliamdias <mrwilliamdias@gmail.com>
2021-03-31 17:56:57 -0300

    Pacotes Externos

--- Pacotes/main.go
+++ Pacotes/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"modulo/Auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Writing on main archive")
 	Auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("116")
+	fmt.Println(erro)
 }
